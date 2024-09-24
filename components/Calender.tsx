6dbd17edb9ae8718f704bc83d4a548522d420ddb
SYED AMEER JUNAID <junaid0825@gmail.com>
2024-09-24 14:18:35 +0530

    six

--- components/Calender.tsx
+++ components/Calender.tsx
@@ -34,7 +34,6 @@ export function DatePicker() {
                     mode="single"
                     selected={date}
                     onSelect={setDate}
-                    initialFocus
                 />
             </PopoverContent>
         </Popover>
